99548e6319da812aa126f5d0322ace6d80cce45e
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-11 22:34:20 +0600

    get data by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,6 +35,12 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/jobs/:id', async(req, res)=>{
+        const id  = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await jobsCollection.findOne(query)
+        res.send(result)
+    })
 
 
 
