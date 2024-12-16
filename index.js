735a7b851e79754188391ebe193d35d5cf51f2cb
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-16 10:34:48 +0600

    verify token with custom middleware

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+
+const logger = (req, res, next) => {
+  console.log("inside the logger");
+  next();
+};
+const verifyToken = (req, res, next) => {
+  console.log("inside the veryfiToken middleware", req.cookies);
+  const token = req?.cookies?.token;
+
+  if (!token) {
+    return res.status(401).send({ message: "unAuthorized access" });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({message: 'unAuthorized access'})
+    }
+    req.user = decoded
+    next()
+  });
+
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ggzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -32,14 +61,21 @@ async function run() {
       .collection("job_applications");
 
     // Auth Related APIs
-    app.post('/jwt', async(req, res) => {
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, 'secret', {expiresIn: '1h'});
-      res.send(token)
-    })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
 
-
-    app.get("/jobs", async (req, res) => {
+    app.get("/jobs", logger, async (req, res) => {
+      console.log("inside api callback");
       const email = req.query.email;
       let query = {};
       if (email) {
@@ -65,9 +101,15 @@ async function run() {
 
     // job application apis
 
-    app.get("/job-application", async (req, res) => {
+    app.get("/job-application", verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { applicant_email: email };
+
+      // verifying token
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({message: 'Forbiden access'})
+      }
+
       const result = await jobApplicationCollection.find(query).toArray();
 
       // not the best way to do it
